5d43d0027ed6cf52e108836fe880e1f2ef6ade9c
nicjacks315 <80596121+nicjacks315@users.noreply.github.com>
2021-04-18 22:38:26 -0700

    Update aquaponics.hpp

--- Vertical-Farming/aquaponics.hpp
+++ Vertical-Farming/aquaponics.hpp
@@ -43,26 +43,9 @@ private :
 };
 
 //-------------------------------------------------------------------------------------------------------------------------------------
-//implementation of sensor class for Total Dissolved Solids Sensor
-class SensorTds : public Sensor {
-public :
-  SensorTds( unsigned long p, byte i, float* f ) : Sensor(p), m_pPpm(f), m_pinIn(i) {}
-  void init() {
-    pinMode( m_pinIn, INPUT );
-  }
-  
-protected :
-  void poll() {
-    *m_pPpm = analogRead( m_pinIn );
-  }
-
-private :
-  float* m_pPpm;
-  byte m_pinIn;
-};
 
 //-------------------------------------------------------------------------------------------------------------------------------------
-//implementation of sensor class for Total Dissolved Solids Sensor
+//implementation of pH sensor class
 class SensorPh : public Sensor {
 public :
   SensorPh( unsigned long p, byte i, float* f ) : Sensor(p), m_pAcidity(f), m_pinIn(i) {}
@@ -80,9 +63,8 @@ private :
   byte m_pinIn;
 };
 
-
 //-------------------------------------------------------------------------------------------------------------------------------------
-//implementation of sensor class for Total Dissolved Solids Sensor
+//implementation of sensor class for pwm flowmeter
 class SensorFlow : public Sensor {
 public :
 
@@ -123,12 +105,87 @@ static void SensorFlow_Rising() {
 static void SensorFlow_Falling() {
   SensorFlow::k_pwmValue = micros()-SensorFlow::k_prevTime;
   attachInterrupt(digitalPinToInterrupt(SensorFlow::k_pinIn), &SensorFlow_Rising, RISING);
-
 }
 
 static void SensorFlow::init() {
   pinMode(SensorFlow::k_pinIn, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(SensorFlow::k_pinIn), &SensorFlow_Rising, RISING);
 }
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+//implementation of sensor class for tds and ppm
+
+#define VREF 5.0 // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30 // sum of sample point
+
+
+class SensorTds : public Sensor {
+public :
+  SensorTds( unsigned long p, byte i, float* f ) : Sensor(p), m_pTds(f), m_pinIn(i) {}
+  void init() {
+    pinMode( m_pinIn, INPUT );
+  }
+  
+protected :
+  void poll() {
+
+    m_analogBuffer[m_analogBufferIndex] = analogRead(m_pinIn); //read the analog value and store into the buffer
+    m_analogBufferIndex++;
+    if( m_analogBufferIndex == SCOUNT ) {
+      m_analogBufferIndex = 0;
+    }
+      
+    for( m_copyIndex = 0; m_copyIndex < SCOUNT; m_copyIndex++ ) {
+      m_analogBufferTemp[m_copyIndex] = m_analogBuffer[m_copyIndex];
+    }
+      
+    m_averageVoltage = getMedianNum(m_analogBufferTemp,SCOUNT) * (float)VREF/ 1024.0;       // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    float compensationCoefficient=1.0+0.02*(m_temperature-25.0);                          //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge=m_averageVoltage/compensationCoefficient;                   //temperature compensation
+    *m_pTds=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+  }
+
+private :
+  float* m_pTds;
+  byte m_pinIn;
+
+  int m_analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
+  int m_analogBufferTemp[SCOUNT];
+  int m_analogBufferIndex = 0;
+  int m_copyIndex = 0;
+  float m_averageVoltage = 0;
+  float m_temperature = 25;
+};
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+//implementation of sensor class for water temperature sensor
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+class SensorWaterTemp : public Sensor {
+public :
+  SensorWaterTemp( unsigned long p, byte i, float* f ) : Sensor(p), m_pWaterTemp(f), m_pinIn(i), m_oneWire(i), m_sensors(&m_oneWire) {}
+  void init() {
+    m_sensors.begin();
+  }
+  
+protected :
+  void poll() {
+    m_sensors.requestTemperatures();
+    *m_pWaterTemp = m_sensors.getTempCByIndex(0);
+  }
+
+private :
+  float* m_pWaterTemp;
+  byte m_pinIn;
+  OneWire m_oneWire;
+  DallasTemperature m_sensors;
+
+};
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
 
 #endif
